a912598e03163ca0a92ddc9edd900f19852ce5c1
Gil Sousa <tiagosousafl@gmail.com>
2018-01-06 13:27:51 +0000

    Refactor code into a class

--- gbook.py
+++ gbook.py
@@ -5,58 +5,68 @@ from bs4 import BeautifulSoup
 import time
 import re
 
+class FacebookBot:
+    def __init__(driver):
+        self.driver = driver
 
-# Post
-post = """ Sample Post """ # change this to what you will post to each group
-interval = 500 # amount of time to wait before posting to next group
+    def login(username, password):
+        self.driver.get("https://mbasic.facebook.com")
 
-# Login information for Facebook
-usr = input("Username: ")
-pwd = input("Password: ")
+        # Login to facebook
+        print("Logging into facebook...")
+        usr_elem = self.driver.find_element_by_css_selector('#m_login_email')
+        pwd_elem = self.driver.find_element_by_css_selector(
+            '#login_form > ul > li:nth-child(2) > div > input')
 
-# Initiate webdriver - PhantomJS
-print("Iniatiating PhantomJS")
-driver = webdriver.PhantomJS()
+        usr_elem.send_keys(usr)
+        pwd_elem.send_keys(pwd)
+        pwd_elem.send_keys(Keys.RETURN)
+        time.sleep(2)  # We'll wait 2 seconds for the page to load completely
 
-# We'll use this url for easy element targeting
-driver.get("https://mbasic.facebook.com")
+    def collectGroups():
+        print("Collecting groups in profile...")
+        self.driver.get('https://mbasic.facebook.com/groups/?seemore&refid=27')
 
-# Login to facebook
-print("Logging into facebook...")
-usr_elem = driver.find_element_by_css_selector('#m_login_email')
-pwd_elem = driver.find_element_by_css_selector(
-    '#login_form > ul > li:nth-child(2) > div > input')
+        groups = []
+        source = self.driver.page_source
+        soup = BeautifulSoup(source, "html.parser")
 
-usr_elem.send_keys(usr)
-pwd_elem.send_keys(pwd)
-pwd_elem.send_keys(Keys.RETURN)
-time.sleep(2)  # We'll wait 2 seconds for the page to load completely
+        for group in soup.find_all("a", href=re.compile(r"groups/\d")):
+            groups.append("https://mbasic.facebook.com" + group['href'])
 
-# Collect all groups profile
-print("Collecting groups in profile...")
-driver.get('https://mbasic.facebook.com/groups/?seemore&refid=27')
+        print(f"{len(groups)} groups found in profile.")
 
-groups = []
-source = driver.page_source
-soup = BeautifulSoup(source, "html.parser")
+        return groups
 
-for group in soup.find_all("a", href=re.compile(r"groups/\d")):
-    groups.append("https://mbasic.facebook.com" + group['href'])
+    def postToGroups(post, interval, groups):
+        # Go to each group and post
+        for i, group in enumerate(groups):
+            self.driver.get(group)
+            try:
+                post_elem = self.driver.find_element_by_css_selector('#u_0_0')
+                sub_elem = self.driver.find_element_by_name('Post')
 
-print(f"{len(groups)} groups found in profile.")
+                post_elem.send_keys(post)
+                sub_elem.click()
 
-# Go to each group and post
-for i, group in enumerate(groups):
-    driver.get(group)
-    try:
-        post_elem = driver.find_element_by_css_selector('#u_0_0')
-        sub_elem = driver.find_element_by_name('Post')
+                print(f"Sucessfully posted in group #{i}.")
+                time.sleep(interval)
+            except NoSuchElementException:
+                print(f"Can't post in group #{i}.")
+                continue
 
-        post_elem.send_keys(post)
-        sub_elem.click()
+if __name__ == '__main__':
+    usr = input('Username: ')
+    pwd = input('Password: ')
 
-        print(f"Sucessfully posted in group #{i}.")
-        time.sleep(interval)
-    except NoSuchElementException:
-        print(f"Can't post in group #{i}.")
-        continue
+    # Initiate webdriver - PhantomJS
+    print("Iniatiating PhantomJS")
+    driver = webdriver.PhantomJS()
+
+    myBot = new FacebookBot(driver)
+    myBot.login(usr, pwd)
+
+    groups = myBot.collectGroups()
+
+    post = """ Sample Post """ # change this to what you will post to each group
+    myBot.postToGroups(post, 500, groups)
